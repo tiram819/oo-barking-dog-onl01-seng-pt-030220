ae462e2340d16180174840609d5c7181026ffcc7
Tabassum Hassan <tiram819@gmail.com>
2020-03-15 23:47:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Dog
+  
+  
+  class Dog
+  def name=(dogs_name)
+    @has_a_name = dogs_name
+  end 
+
+
+
+def name
+    @has_a_name
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"  
+
+puts fido.name
+
+
+
+def bark
+    puts "woof!"
+  end 
+end
+
+
+
